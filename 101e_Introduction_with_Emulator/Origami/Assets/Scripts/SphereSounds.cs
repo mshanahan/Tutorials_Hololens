94de261de6c06638d2dbf208db113f9713967449
Michael Shanahan <michael.shanahan5@gmail.com>
2017-12-01 16:23:40 -0600

    hear holograms in the world

---
+++ 101e_Introduction_with_Emulator/Origami/Assets/Scripts/SphereSounds.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class SphereSounds : MonoBehaviour
+{
+    AudioSource impactAudioSource = null;
+    AudioSource rollingAudioSource = null;
+
+    bool rolling = false;
+
+    void Start()
+    {
+        // Add an AudioSource component and set up some defaults
+        impactAudioSource = gameObject.AddComponent<AudioSource>();
+        impactAudioSource.playOnAwake = false;
+        impactAudioSource.spatialize = true;
+        impactAudioSource.spatialBlend = 1.0f;
+        impactAudioSource.dopplerLevel = 0.0f;
+        impactAudioSource.rolloffMode = AudioRolloffMode.Logarithmic;
+        impactAudioSource.maxDistance = 20f;
+
+        rollingAudioSource = gameObject.AddComponent<AudioSource>();
+        rollingAudioSource.playOnAwake = false;
+        rollingAudioSource.spatialize = true;
+        rollingAudioSource.spatialBlend = 1.0f;
+        rollingAudioSource.dopplerLevel = 0.0f;
+        rollingAudioSource.rolloffMode = AudioRolloffMode.Logarithmic;
+        rollingAudioSource.maxDistance = 20f;
+        rollingAudioSource.loop = true;
+
+        // Load the Sphere sounds from the Resources folder
+        impactAudioSource.clip = Resources.Load<AudioClip>("Impact");
+        rollingAudioSource.clip = Resources.Load<AudioClip>("Rolling");
+    }
+}
